4bede7205622bfc5a3caa8ac3d2099aab70def53
Michael Sawula <msawula@users.noreply.github.com>
2016-11-07 23:14:16 -0600

    3 out of 4 complete

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -2,4 +2,27 @@ class CalculationsController < ApplicationController
   def instructions
     render("instructions.html.erb")
   end
+
+  def squaring
+    @number = params[:side_length]
+    @squarednumber = @number.to_i * @number.to_i
+    render("show_square.html.erb")
+  end
+
+  def square_root
+    @number = params[:value]
+    @sqrtroot = Math.sqrt(@number.to_i)
+    render("show_squareroot.html.erb")
+  end
+
+def random
+@lowernumber = params[:lower]
+@uppernumber = params[:upper]
+
+@selection = rand(@lowernumber.to_i..@uppernumber.to_i)
+render("show_random.html.erb")
+end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   get("/", { :controller => "calculations", :action => "instructions" })
+
+  #Square of a particular number
+  get("/square/:side_length", { :controller => 'calculations', :action => 'squaring' })
+
+#Square root of a particular number
+  get("/square_root/:value", { :controller => 'calculations', :action => 'square_root' })
+
+#Random number
+get("/random/:lower/:upper", { :controller => 'calculations', :action => 'random' })
+
 end
